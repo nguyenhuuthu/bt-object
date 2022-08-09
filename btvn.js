54dd8adfc7f4ca02e9b02bad39a1e827041b0de0
nguyenhuuthu <110140882+nguyenhuuthu@users.noreply.github.com>
2022-08-09 10:19:22 +0700

    Add files via upload

--- btvn.js
+++ btvn.js
@@ -63,44 +63,194 @@
 // }
 
 //bài 2
-let mission = [
-    {
-        1: "html",
-        hoanthanh: "false",
-    },
-    {
-        2: "css",
-        hoanthanh: "false",
-    },
-    {
-        3: "js",
-        hoanthanh: "false",
-    },
-    {
-        4: "node package mannager",
-        hoanthanh: "false",
-    },
-    {
-        5: "git",
-        hoanthanh: "false",
-    }
-]
-console.log(mission);
+// let mission = [
+//     {
+//         id1: "html",
+//         hoanthanh: "false",
+//     },
+//     {
+//         id2: "css",
+//         hoanthanh: "false",
+//     },
+//     {
+//         id3: "js",
+//         hoanthanh: "false",
+//     },
+//     {
+//         id4: "node package mannager",
+//         hoanthanh: "false",
+//     },
+//     {
+//         id5: "git",
+//         hoanthanh: "false",
+//     }
+// ]
+// console.log(mission);
 
 //2.2
-let userInput = prompt('nhap vao c/r/u/d');
-if( userInput == "c") {
-    input = prompt('nhap vao nhiem vu moi ');
-    for (let i = 0; i < mission.length; i++) {
-        mission[i].input = "hoan thanh :false"
-        console.log(mission);
+// let userInput = prompt('nhap vao c/r/u/d');
+// if( userInput == "c") {
+//     input = prompt('nhap vao nhiem vu moi ');
+//     for (let i = 0; i < mission.length; i++) {
+//         mission[i].input = "hoan thanh :false"
+//         console.log(mission);
+//     }
+
+// }else if( userInput == "u") {
+//     let a = prompt('nhap vao vi tri muon thay doi');
+//     let b = prompt('nhap vao tieu de muon thay doi');
+// objIndex = mission.findIndex((obj => obj.id == a));
+
+// console.log("Before update: ", mission[objIndex])
+
+// mission[objIndex].id2 = "hoanthanh :false"
+
+// console.log("After update: ", mission[objIndex])
+// }
+
+//2 + 2.6
+let learning = [
+    {
+      id: 1,
+      name: "HTML",
+      complete: "false",
+      check: "[ ]",
+    },
+    {
+      id: 2,
+      name: "CSS",
+      complete: "false",
+      check: "[ ]",
+    },
+    {
+      id: 3,
+      name: "Basic of javascript",
+      complete: "false",
+      check: "[ ]",
+    },
+    {
+      id: 4,
+      name: "Node package Manager (npm)",
+      complete: "false",
+      check: "[ ]",
+    },
+    {
+      id: 5,
+      name: "Git",
+      complete: "false",
+      check: "[ ]",
+    },
+  ];
+  
+  //2.2 add new
+  while (true) {
+    let command = prompt("Enter your command(New, Delete, Update, Complete)");
+    if (command == "new") {
+      let taskInput = prompt("Enter new task:");
+      learning.push({
+        id: learning.length + 1,
+        name: taskInput,
+        complete: false,
+        check: "[ ]",
+      });
+      console.clear();
+  
+      for (let key in learning) {
+        console.log(
+          learning[key].id +
+            ". " +
+            learning[key].name +
+            "\n" +
+            "Complete: " +
+            learning[key].complete
+        );
+      }
+      console.log("--------------------");
+  
+      for (let key in learning) {
+        console.log(
+          learning[key].id + ". " + learning[key].check + " " + learning[key].name
+        );
+      } //2.3 update
     }
-
-}else if( userInput == "u") {
-    let a = prompt('nhap vao vi tri muon thay doi');
-    let b = prompt('nhap vao tieu de muon thay doi');
-for (let index = 0; index < mission.length; index++) {
-
-}
-}
-// đến đây e chưa làm ddc
+    if (command == "update") {
+      let position = Number(prompt("Enter position "));
+      let title = prompt("Enter new title");
+      for (let key in learning) {
+        if (position == learning[key].id) {
+          learning[key].name = title;
+          console.log(learning[key].name);
+        }
+      }
+      console.clear();
+      for (let key in learning) {
+        console.log(
+          learning[key].id +
+            ". " +
+            learning[key].name +
+            "\n" +
+            "Complete: " +
+            learning[key].complete
+        );
+      }
+      console.log("-------------------------------");
+      for (let key in learning) {
+        console.log(
+          learning[key].id + ". " + learning[key].check + " " + learning[key].name
+        );
+      }
+    }
+    //2.4 complete
+    if (command == "complete") {
+      let position = Number(prompt("Nhập vị trí để hoàn thành"));
+      for (let key in learning) {
+        if (position == learning[key].id) {
+          learning[key].complete = true;
+          learning[key].check = "[x]";
+        }
+      }
+      console.clear();
+      for (let key in learning) {
+        console.log(
+          learning[key].id +
+            ". " +
+            learning[key].name +
+            "\n" +
+            "Complete: " +
+            learning[key].complete
+        );
+      }
+      console.log("----------------------------------------------------------");
+      for (let key in learning) {
+        console.log(
+          learning[key].id + ". " + learning[key].check + " " + learning[key].name
+        );
+      }
+    }
+    if (command == "d") {
+      let position = Number(prompt("Nhập vị trí muốn xóa"));
+      for (let key in learning) {
+        if (position == learning[key].id) {
+          delete learning[key];
+        }
+      }
+      console.clear();
+      for (let key in learning) {
+        console.log(learning[key].id +
+            ". " +
+            learning[key].name +
+            "\n" +
+            "Complete: " +
+            learning[key].complete
+        );
+      }
+      console.log("----------------------------------------------------------");
+      for (let key in learning) {
+        console.log(
+          learning[key].id + ". " + learning[key].check + " " + learning[key].name
+        );
+      }
+    } else if (command == "e") {
+      break;
+    }
+  }
